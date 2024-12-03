868dca1587a02553f15015ac822721d880c0d84d
Rutuja1923 <rutujajirage19@gmail.com>
2024-12-03 06:02:58 +0530

    getting all necessary divs

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const mainContainer = document.querySelector('.main-container');
+    const formDiv = document.querySelector('.form-div');
+    const filtersDiv = document.querySelector('.filters-div');
+    const tasksContainer = document.querySelector('.tasks-container');
+    const pendingTasks = document.querySelector('.pending-tasks-container');
+    const completedTasks = document.querySelector('.completed-tasks-container');
+    
+});
