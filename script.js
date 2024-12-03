21d1366a2a8e1e8a23d5c0c87fb9396e11751af1
Rutuja1923 <rutujajirage19@gmail.com>
2024-12-03 06:06:33 +0530

    new-task-btn and close-task-form-btn

--- script.js
+++ script.js
@@ -10,6 +10,23 @@ document.addEventListener('DOMContentLoaded', () => {
     //adding date 
     mainContainer.querySelector('h4').innerText = `${getToday()}`;
 
+    //new-task-button
+    const newTaskBtn = document.getElementById('new-task-btn');
+    newTaskBtn.addEventListener('click', () => {
+        console.log('clicked new task btn');
+        if (formDiv.style.display === 'none') {
+            formDiv.style.display = 'block';
+        }
+    });
+
+    //close - form - button
+    const closeFormtBtn = document.getElementById('close-form');
+    closeFormtBtn.addEventListener('click' , () => {
+        if (formDiv.style.display === 'block') {
+            formDiv.style.display = 'none';
+        }
+    });
+
 });
 
 //utility functions
