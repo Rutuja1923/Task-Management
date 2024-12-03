402dfca286d7e3ea3f0bb7c2ae5e72d9367c4b4f
Rutuja1923 <rutujajirage19@gmail.com>
2024-12-03 06:03:33 +0530

    added utility functions

--- script.js
+++ script.js
@@ -6,5 +6,26 @@ document.addEventListener('DOMContentLoaded', () => {
     const tasksContainer = document.querySelector('.tasks-container');
     const pendingTasks = document.querySelector('.pending-tasks-container');
     const completedTasks = document.querySelector('.completed-tasks-container');
-    
+
 });
+
+//utility functions
+//todays - date
+const getTodaysDate = () => {
+    const today = new Date();
+    const date = String(today.getDate()).padStart(2 , '0');
+    const mon = String(today.getMonth() + 1 ).padStart(2 , '0');
+    const year = today.getFullYear() ;
+    return `${year}-${mon}-${date}` ;
+}
+//todays date -> day date month year
+const getToday = () => {
+    const dayNames = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    const monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    const today = new Date();
+    const date = today.getDate();
+    const day = dayNames[today.getDay()];
+    const mon = monthNames[today.getMonth()];
+    const year = today.getFullYear();
+    return `${day}, ${date} ${mon} ${year}`;
+}
